8a0b50b24887c30cb730557ab17a98a10d694742
mahrokhazarkhosh <mahrokh.azarkhosh@outlook.com>
2024-11-18 18:45:59 +0700

    label be set in form

--- me.py
+++ me.py
@@ -8,4 +8,13 @@ win = tkinter.Tk()
 win.title("Calculator")
 win.geometry("500x300")
 
+#------lbl-----
+
+label = tkinter.Label(win, text="Calculator")
+label.pack()
+E1 = tkinter.Entry(win)
+E1.pack()
+E2 = tkinter.Entry(win)
+E2.pack()
+
 #
